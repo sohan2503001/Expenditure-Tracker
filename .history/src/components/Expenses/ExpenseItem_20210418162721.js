3321a58b0af279779e4b73cf6a1f3bb7539d06f2
sohan2503001 <sohanjambhule973@gmail.com>
2021-04-18 16:32:34 +0530

    learn basic of states

---
+++ .history/src/components/Expenses/ExpenseItem_20210418162721.js
@@ -0,0 +1,27 @@
+import React , { useState } from 'react';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    const [title, setTit] = useState(props.title);
+
+    let title = props.title;
+
+    const clickHandler = () => {
+        title = 'Updated!';
+    };
+
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date} />
+            <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+            <button onClick={clickHandler}>Change Title</button>
+        </Card>
+    );
+}
+
+export default ExpenseItem;;
